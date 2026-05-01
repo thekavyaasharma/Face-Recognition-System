6ff43db01026d2ee3e825a4b23fe38deb224be43
kavya <kavyash1804@gmail.com>
2026-05-01 21:55:14 +0530

    fixed image texture ratio

--- Facial Recognition app/FaceId.py
+++ Facial Recognition app/FaceId.py
@@ -27,13 +27,13 @@ class CamApp(App): #inheritence
     def build(self):
 
         # define main layout components
-        self.img1 = Image(size_hint=(1,0.8)) # main input image
+        self.web_cam = Image(size_hint=(1,0.8)) # main input image
         self.button = Button(text='Verify', size_hint=(1,0.1))
         self.verification = Label(text = 'verification un-initiated', size_hint=(1,0.1))
 
         # add components and test the layout 
         layout = BoxLayout(orientation = 'vertical')
-        layout.add_widget(self.img1)
+        layout.add_widget(self.web_cam)
         layout.add_widget(self.button)
         layout.add_widget(self.verification)
 
@@ -46,6 +46,20 @@ class CamApp(App): #inheritence
 
     #run continuously to get webcam feed
     def update(self,*args):
+
+        #read frame(numpy array)from opencv
+        ret , frame = self.capture.read()
+        frame = [120:120+250, 200:200+250, : ]
+
+        # flip horizontal and convert image to texture
+        buf = cv2.flip(frame,0).tostring() # flip image horizlly -> cnvrt to string
+        img_texture = Texture.create(size=(frame.shape[1], frame.shape[0]), colorfmt = 'bgr')
+        img_texture.blit_buffer(buf, colorfmt ='bgr', bufferfmt = 'ubyte')
+        self.web_cam.texture = img_texture
+
+
+
+
 
 
 
