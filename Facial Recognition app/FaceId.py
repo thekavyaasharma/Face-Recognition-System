ce9dbd9b76562e4d2e97caf4835cb69b500fc074
kavya <kavyash1804@gmail.com>
2026-05-02 23:54:36 +0530

    frame visibility resolved

--- Facial Recognition app/FaceId.py
+++ Facial Recognition app/FaceId.py
@@ -57,6 +57,7 @@ class CamApp(App): #inheritence
         # flip horizontal and convert image to texture
         buf = cv2.flip(frame,0).tostring() # flip image horizlly -> cnvrt to string
         img_texture = Texture.create(size=(frame.shape[1], frame.shape[0]), colorfmt = 'bgr')
+        
         # img -> convert it into texture -> rencer it inside app
         img_texture.blit_buffer(buf, colorfmt ='bgr', bufferfmt = 'ubyte')
 
